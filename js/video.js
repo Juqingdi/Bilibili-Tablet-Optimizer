0e8ee493a7f7c79f8c44edf3352fefc2b01ebbc7
FallingStarJQD <Grainufo2014@outlook.com>
2018-09-25 23:21:25 +0800

    页面框架
    页面框架
    工具栏假按钮

--- js/video.js
+++ js/video.js
@@ -12,6 +12,8 @@ function Main() {
 	let $bilibiliPlayer = $("#bilibiliPlayer", $bofqi);
 	// console.log($bofqi.prop('outerHTML'));
 
+	CreateFramework();
+
 	let observer = new MutationObserver((records)=>{
 		$("#app .float-nav, #entryOld, #live_recommand_report, #slide_ad").remove();
 		RemoveHeader();
@@ -36,41 +38,111 @@ function Main() {
 	});
 }
 
-function Decorate() {
+function CreateFramework() {
+	console.info($('#v_desc').prop('innerHTML'));
 
 	$container = $(`
-		<div id="BT-videopage">
-			<div class="l-con">
-				<div id="bofqi_container" class="container">
-					<div class="pop-panel">
-						<div class="popout"></div>
-						<div class="popin"></div>
-					</div>
+	<div id="BT-videopage">
+		<div class="l-con">
+			<div id="bofqi_container" class="container">
+				<div class="pop-panel">
+					<div class="popout"></div>
+					<div class="popin"></div>
 				</div>
-				<div id="arc_toolbar_report_container" class="container"></div>
-				<div id="BT-info" class="container">
-					<div id="viewbox_report_container"></div>
-					<div id="v_desc_container"></div>
-					<button class="BT-more"></button>
-				</div>
-				<div id="multi_page_container" class="container"></div>
-				<div id="v_tag_container" class="container"></div>
 			</div>
-			<div class="r-con">
-				<div id="v_upinfo_container" class="container"></div>
-				<div id="BT-switch_tag" class="comment-active container"">
-					<span class="BT-comment"><span class="comment-total"></span> 评论</span>
-					<span class="BT-reco">相关视频推荐</span>
-				</div>
-				<div id="comment_reco">
-					<div class="scroll">
-						<div id="comment_container" class="container"></div>
-						<div id="reco_list_container" class="container"></div>
+			<div id="arc_toolbar_report_container" class="container">
+				<div id="arc_toolbar_report" class="video-toolbar report-wrap-module report-scroll-module">
+					<div class="ops">
+						<span class="like"><i class="van-icon-videodetails_like"></i>--</span>
+						<span class="coin"><i class="van-icon-videodetails_throw"></i>--</span>
+						<span class="collect"><i class="van-icon-videodetails_collec"></i>--</span>
+						<div class="more-ops-list">
+							<ul>
+								<li>
+									<div class="van-watchlater van-watchlater-icon"></div>
+									<div class="ops-watch-later van-watchlater"></div>
+									稍后再看
+								</li>
+							</ul>
+						</div>
 					</div>
+				</div>
+			</div>
+			<div id="BT-info" class="container">
+				<div id="viewbox_report_container"></div>
+				<div id="v_desc_container"></div>
+				<button class="BT-more"></button>
+			</div>
+			<div id="multi_page_container" class="container"></div>
+			<div id="v_tag_container" class="container"></div>
+		</div>
+		<div class="r-con">
+			<div id="v_upinfo_container" class="container"></div>
+			<div id="BT-switch_tag" class="comment-active container"">
+				<span class="BT-comment"><span class="comment-total"></span> 评论</span>
+				<span class="BT-reco">相关视频推荐</span>
+			</div>
+			<div id="comment_reco">
+				<div class="scroll">
+					<div id="comment_container" class="container"></div>
+					<div id="reco_list_container" class="container"></div>
 				</div>
 			</div>
 		</div>
-		`);
+	</div>
+	`);
+	$('body').append($container);
+
+	$bofqiContainer = $('#bofqi_container', $container);
+
+	//计算播放器容器高度，宽度足够时显示完整播放器，否则遮挡住弹幕输入框
+	if( $bofqiContainer.width() < bofqiWidth){
+		$bofqiContainer.addClass('small-player');
+		let bofqiContainerWidth = $bofqiContainer.width();
+		$bofqiContainer.height( bofqiContainerWidth * 9 /16 );
+		let $bofqiPanel = $(".pop-panel", $bofqiContainer);
+		let scale = bofqiContainerWidth / bofqiWidth;
+		$bofqiPanel.css('zoom', scale);
+
+		let $popout = $(".popout", $bofqiContainer);
+		let $popin = $(".popin", $bofqiContainer);
+		$popout.click(()=>{
+			$bofqiPanel.addClass('poped');
+			$bofqiPanel.css({
+				'position': 'fixed',
+				'transform': `scale(${scale}) translate(${60 / scale}px, ${$bofqiContainer.offset().top / scale}px)`,
+				'zoom': 0
+			});
+			setTimeout(() => {
+				$bofqiPanel.css({
+					'transform': `translate( ${(window.innerWidth - bofqiWidth) / 2}px, ${(window.innerHeight - bofqiHeight) / 2}px )`,
+				    'transition': 'transform 0.5s'
+				});
+			}, 0);
+		});
+
+		$popin.click(()=>{
+			$bofqiPanel.removeClass('poped');
+			$bofqiPanel.css('transform', `scale(${scale}) translate(${60 / scale}px, ${$bofqiContainer.offset().top / scale}px)`);
+		});
+		$bofqiPanel.on('transitionend', (e)=>{
+			if(e.currentTarget != e.target)
+				return;
+			if(!$bofqiPanel.hasClass('poped')){
+				$bofqiPanel.css({
+					'transform': 'none',
+				    'transition': 'none',
+				    'position': 'static',
+				    'zoom': scale
+				});
+			}
+		})
+	}
+	else
+		$bofqiContainer.height( bofqiHeight);
+}
+
+function Decorate() {
 
 	//播放器
 	$bofqi = $("#bofqi");
@@ -130,63 +202,13 @@ function Decorate() {
 	$("#bofqi").prependTo($('#bofqi_container .pop-panel', $container));
 	$viewboxReport.appendTo($("#viewbox_report_container", $container));
 	// console.log($("#arc_toolbar_report").prop('outerHTML'));
-	$toolbar.appendTo($('#arc_toolbar_report_container', $container));
+	$("#arc_toolbar_report_container", $container).empty().append( $toolbar);
 	$("#v_tag").appendTo($('#v_tag_container', $container));
 	$("#v_desc").appendTo($('#v_desc_container', $container));
 	$("#comment").appendTo($('#comment_container', $container));
 	$("#v_upinfo").appendTo($('#v_upinfo_container', $container));
 	$("#multi_page").appendTo($('#multi_page_container', $container));
 	$("#reco_list").appendTo($recoListContainer);
-
-	$bofqiContainer = $('#bofqi_container', $container);
-
-	$('body').append($container);
-
-	//计算播放器容器高度，宽度足够时显示完整播放器，否则遮挡住弹幕输入框
-	if( $bofqiContainer.width() < bofqiWidth){
-		$bofqiContainer.addClass('small-player');
-		let bofqiContainerWidth = $bofqiContainer.width();
-		$bofqiContainer.height( bofqiContainerWidth * 9 /16 );
-		let $bofqiPanel = $(".pop-panel", $bofqiContainer);
-		let scale = bofqiContainerWidth / bofqiWidth;
-		$bofqiPanel.css('zoom', scale);
-
-		let $popout = $(".popout", $bofqiContainer);
-		let $popin = $(".popin", $bofqiContainer);
-		$popout.click(()=>{
-			$bofqiPanel.addClass('poped');
-			$bofqiPanel.css({
-				'position': 'fixed',
-				'transform': `scale(${scale}) translate(${60 / scale}px, ${$bofqiContainer.offset().top / scale}px)`,
-				'zoom': 0
-			});
-			setTimeout(() => {
-				$bofqiPanel.css({
-					'transform': `translate( ${(window.innerWidth - bofqiWidth) / 2}px, ${(window.innerHeight - bofqiHeight) / 2}px )`,
-				    'transition': 'transform 0.5s'
-				});
-			}, 0);
-		});
-
-		$popin.click(()=>{
-			$bofqiPanel.removeClass('poped');
-			$bofqiPanel.css('transform', `scale(${scale}) translate(${60 / scale}px, ${$bofqiContainer.offset().top / scale}px)`);
-		});
-		$bofqiPanel.on('transitionend', (e)=>{
-			if(e.currentTarget != e.target)
-				return;
-			if(!$bofqiPanel.hasClass('poped')){
-				$bofqiPanel.css({
-					'transform': 'none',
-				    'transition': 'none',
-				    'position': 'static',
-				    'zoom': scale
-				});
-			}
-		})
-	}
-	else
-		$bofqiContainer.height( bofqiHeight);
 
 	//切换视频时，评论数清零，相关视频复位，视频信息收起 
 	let titleObserver = new MutationObserver((records)=>{
