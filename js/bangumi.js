c6f68af8bfe446ef0ab8cdee3bb49da3113d2f71
Juqingdi <grain2011@sina.com>
2018-09-30 22:19:07 +0800

    番剧页

---
+++ js/bangumi.js
@@ -0,0 +1,10 @@
+function Main() {
+	// $('<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">').prependTo($("head"));
+
+	SetSideMenu();
+
+	$(".BT-iconfont", $sideMenu).css('cssText', 'font-family:"BT-iconfont" !important;');
+}
+
+if(true)
+	Main();
